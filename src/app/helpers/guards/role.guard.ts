fce230c00be3b2b915adcf9d5ce88d6929a1e904
VicenteM325 <vicente.matias.161@gmail.com>
2025-10-28 03:51:34 -0600

    A

--- src/app/helpers/guards/role.guard.ts
+++ src/app/helpers/guards/role.guard.ts
@@ -6,35 +6,29 @@ import { firstValueFrom } from 'rxjs';
 export const roleGuard: CanActivateFn = async (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-
   const expectedRole = route.data['expectedRole'];
+
+  if (!authService.getIsInitialized()) {
+    await firstValueFrom(authService.initializeAuth());
+  }
+
   let user = authService.getCurrentUser();
 
-  try {
-    if (!user) {
-      const restoredUser = await firstValueFrom(authService.getDetails());
-      if (!restoredUser) {
-        await router.navigate(['/login']);
-        return false;
-      }
-      user = restoredUser;
-    }
-
-    const userRole = user.role?.name;
-
-    const hasAccess = Array.isArray(expectedRole)
-      ? expectedRole.includes(userRole)
-      : userRole === expectedRole;
-
-    if (hasAccess) {
-      return true;
-    } else {
-      await router.navigate(['/404']);
-      return false;
-    }
-
-  } catch (error) {
+  if (!user) {
     await router.navigate(['/login']);
+    return false;
+  }
+
+  const userRole = user.role?.name;
+
+  const hasAccess = Array.isArray(expectedRole)
+    ? expectedRole.includes(userRole)
+    : userRole === expectedRole;
+
+  if (hasAccess) {
+    return true;
+  } else {
+    await router.navigate(['/404']);
     return false;
   }
 };
